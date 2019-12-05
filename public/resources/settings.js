913732709c7154f395e7c61769ccf8891209b4e8
Jaskaran Singh <sabby97@users.noreply.github.com>
2019-12-04 19:36:41 -0700

    I can contact the server

--- public/resources/settings.js
+++ public/resources/settings.js
@@ -12,13 +12,32 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var firestore = firebase.firestore();
 
-//const submitButton = document.getElementById('settings_form');
+//var submitButton = document.getElementById('settings_form');
 
-function SettingsFunction(form){
-  var docUpdate = firestore.collection("users").doc("VNHuSz5sqhgDeXcusL6a");
-  docUpdate.update({
-    email: form.new_email.value,
-    password: form.confirm_password.value,
-    zipcode: form.new_zip.value
-  });
-};
+//values from the the webpage
+
+ var new_email = document.getElementById('new_email');
+ var old_password = document.getElementById('old_password');
+ var new_password = document.getElementById('new_password');
+ var confirm_password= document.getElementById("confirm_password");
+ var new_zip = document.getElementById("new_zip");
+ const submit_changes = document.getElementById("submit_changes");
+
+ submit_changes.addEventListener("click", function(){
+   var docUpdate = firestore.collection("users").doc("VNHuSz5sqhgDeXcusL6a");
+   var a = new_email.value;
+   var b = confirm_password.value;
+   var c = new_zip.value;
+   console.log(docUpdate);
+   docUpdate.update({
+     email: a,
+     password: b,
+     zipcode: c
+   }).then(function(){
+     alert("That shit worked");
+     console.log(docUpdate);
+   }).catch(function (error){
+     alert("Nope. Didn't work.");
+     console.log('here');
+   });
+ })
