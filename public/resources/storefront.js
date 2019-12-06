10b93eff9e89f38c919f16b156dd2a5aacc855ba
chrisbefin <christian@jba4.com>
2019-12-05 21:27:40 -0700

    send user to login instead of navigation

--- public/resources/storefront.js
+++ public/resources/storefront.js
@@ -33,7 +33,12 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var firestore = firebase.firestore();
 
+if (localStorage.getItem('email') == NULL) {
+  document.getElementById('inventoryLink').href = "../views/BBlogin.html";
+  document.getElementById('profileLink').href = "../views/BBlogin.html";
+  document.getElementById('settingsLink').href = "../views/BBlogin.html";
 
+}
 // the docRef is the specific document in our database you would like to update
 const docRef = firestore.collection("inventory");
 // this is the specific field we want to change
